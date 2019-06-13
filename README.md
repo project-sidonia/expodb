9618277afa9367da8228d24f4c5fc2cfc57031b1
eric <esniff@gmail.com>
2019-06-12 19:14:46 -0700

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# epsniff/expodb
+
+A minimal example of using HashiCorp Raft.
+
+## Video
+
+* https://www.youtube.com/watch?v=EGRmmxVFOfE
+
+## Build
+
+```bash
+$ go get github.com/epsniff/expodb
+$ cd $GOPATH/src/github.com/epsniff/expodb
+$ dep ensure
+$ go build
+```
---
+++ fsm.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"sync"
+
+	"github.com/hashicorp/raft"
+	"github.com/rs/zerolog"
+)
+
+type fsm struct {
+	mutex      sync.Mutex
+	log        *zerolog.Logger
+	stateValue map[string]int
+}
+
+type event struct {
+	Type  string
+	Key   string
+	Value int
+}
+
+// Apply applies a Raft log entry to the key-value store.
+func (fsm *fsm) Apply(logEntry *raft.Log) interface{} {
+	var e event
+	if err := json.Unmarshal(logEntry.Data, &e); err != nil {
+		panic("Failed unmarshaling Raft log entry. This is a bug.")
+	}
+
+	switch e.Type {
+	case "set":
+		fsm.mutex.Lock()
+		defer fsm.mutex.Unlock()
+		fsm.stateValue[e.Key] = e.Value
+		fsm.log.Debug().Int(e.Key, e.Value).Msg("saving key")
+		return nil
+	default:
+		panic(fmt.Sprintf("Unrecognized event type in Raft log entry: %s. This is a bug.", e.Type))
+	}
+}
+
+func (fsm *fsm) Snapshot() (raft.FSMSnapshot, error) {
+	fsm.mutex.Lock()
+	defer fsm.mutex.Unlock()
+
+	return &fsmSnapshot{stateValue: fsm.stateValue}, nil
+}
+
+// Restore stores the key-value store to a previous state.
+func (fsm *fsm) Restore(serialized io.ReadCloser) error {
+	var snapshot fsmSnapshot
+	if err := json.NewDecoder(serialized).Decode(&snapshot); err != nil {
+		return err
+	}
+
+	fsm.stateValue = snapshot.stateValue
+	return nil
+}
